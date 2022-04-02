515e9a0b61d11ad46627c22f806b932e4f1e5b96
MOHAMMED ALOBAIDY <98955907+Engmohammedalobaidy@users.noreply.github.com>
2022-04-02 13:23:37 +0000

    Add files via upload

---
+++ INFO-INSTA-IQ.py
@@ -0,0 +1,59 @@
+import requests
+import pyfiglet
+
+
+# Regular Colors
+Z = '\033[1;31m' #احمر
+X = '\033[1;33m' #اصفر
+Z1 = '\033[2;31m' #احمر ثاني
+F = '\033[2;32m' #اخضر
+A = '\033[2;34m'#ازرق
+C = '\033[2;35m' #وردي
+B = '\033[2;36m'#سمائي
+Y = '\033[1;34m' #ازرق فاتح
+C ='\033[0;36m' # Cyan
+W ='\033[0;37m' # White
+
+print("")
+print(B+" ****************************************************************")
+print("")
+logo = pyfiglet.figlet_format("                 INFO")
+loga = pyfiglet.figlet_format("  INSTAGRAM")
+print(W+logo)
+print(Z+loga)
+print(F+"                     BY: eng.mohammedalobaidy")
+print("")
+
+print(B+" ****************************************************************")
+print("")
+username = input(' \033[0;37m [#] \033[0;36mUserName \033[1;33m: ')
+
+url = f'https://www.instagram.com/{username}/?__a=1'
+r = requests.get(url).json()
+username = r ['graphql']['user']['full_name']
+bio = r ['graphql']['user']['biography']
+profile_pic = r ['graphql']['user']['profile_pic_url_hd']
+posts = r ['graphql']['user']['edge_owner_to_timeline_media']['count']
+followers = r ['graphql']['user']['edge_followed_by']['count']
+following = r ['graphql']['user']['edge_follow']['count']
+print(Z+"-----------------------------------------------------")
+print("")
+print("")
+print("")
+print(f' \033[0;37m[#] \033[0;36mUserName \033[1;33m: {username}')
+print("")
+print(f' \033[0;37m[#] \033[0;36mBio \033[1;33m: {bio}')
+print(f' \033[0;37m[#] \033[0;36mID \033[1;33m: {id}')
+print("")
+print(f' \033[0;37m[#] \033[0;36mPosts \033[1;33m: {posts}')
+print("")
+print(f' \033[0;37m[#] \033[0;36mFollowers \033[1;33m: {followers}')
+print("")
+print(f' \033[0;37m[#] \033[0;36mFollowing \033[1;33m: {following}')
+print("")
+print("")
+print("")
+print(Z+"-----------------------------------------------------")
+pic = requests.get(profile_pic)
+with open('profile profile.png','wb') as f :
+    f.write(pic.content)
